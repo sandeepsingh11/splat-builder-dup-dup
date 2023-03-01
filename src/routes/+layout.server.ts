3703107c810e347f6686ea347d365e5e9658d493
Sandeep Singh <serperior2@protonmail.com>
2023-02-28 22:47:24 -0500

    add navbar

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,25 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
     const { username } = locals;
-
-    return { username };
+    
+    const guestRoutes = [
+        {name: 'Login', url: '/login'},
+        {name: 'Register', url: '/register'},
+    ];
+    const authRoutes = [
+        {name: '+Gear', url: '/gears/create'},
+        {name: '+Gearset', url: '/gearsets/create'},
+    ];
+    const dropdownRoutes = [
+        {name: 'Dashboard', url: '/dashboard'},
+        {name: 'Gears', url: '/gears'},
+        {name: 'Gearsets', url: '/gearsets'},
+    ];
+    
+    return { 
+        username,
+        guestRoutes,
+        authRoutes,
+        dropdownRoutes
+    };
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,86 @@
   import type { LayoutServerData } from "./$types";
 
   export let data: LayoutServerData;
+  export let showDropdown = false;
+
+  function toggleDropdown() {
+    showDropdown = !showDropdown;
+  }
+
+  function logout() {
+
+  }
 </script>
 
-Hello 
-  {#if data?.username}
-    {data.username}
-  {:else}
-    guest
-  {/if}
+<nav class="relative bg-primary-500 text-white flex justify-between">
+  <!-- logo -->
+  <ul class="flex items-center">
+    <li>
+      <a 
+        href="/"
+        class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors"
+      >
+        SB
+      </a>
+    </li>
+  </ul>
+
+  <!-- nav items -->
+  <ul class="md:flex items-center hidden">
+    <!-- guest routes -->
+    {#if !data?.username}
+      {#each data.guestRoutes as route}
+        <a 
+          href="{ route.url }"
+          class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors"
+        >
+          { route.name }
+        </a>
+      {/each}
+    {/if}
+
+    <!-- auth routes -->
+    {#if data?.username}
+      {#each data.authRoutes as route}
+        <a 
+          href="{ route.url }"
+          class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors"
+        >
+          { route.name }
+        </a>
+      {/each}
+
+      <!-- dropdown -->
+      <div
+        on:click={toggleDropdown}
+        on:keyup={toggleDropdown}
+        class="relative"
+      >
+        <button class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors">
+          Profile
+        </button>
+
+        {#if showDropdown}
+          <ul class="absolute top-full right-0 bg-primary-500 mt-1 rounded text-left z-20">
+            {#each data.dropdownRoutes as route}
+              <a 
+                href="{ route.url }"
+                class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors py-2 px-4 rounded-t-md"
+              >
+                { route.name }
+              </a>
+            {/each}
+            <button 
+              class="block p-3 hover:bg-primary-600 focus:hover:bg-primary-600 transition-colors py-2 px-4 rounded-t-md w-full text-left"
+              on:click={logout}
+            >
+              Logout
+            </button>
+          </ul>
+        {/if}
+      </div>
+    {/if}
+  </ul>
+</nav>
 
 <slot />
